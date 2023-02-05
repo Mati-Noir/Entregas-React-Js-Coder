37e6c57edb2c942f631f08b1047466a26a243b2d
Matias Ezequiel Noir <108021175+Mati-Noir@users.noreply.github.com>
2023-02-05 19:31:51 -0300

    Add files via upload

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Titulo from '../Titulo/Titulo'
+
+export const ItemListContainer = (props) => {
+  return (
+    <Titulo greeting="Bienvenido a la pagina"/>
+  )
+}
+
+export default ItemListContainer
